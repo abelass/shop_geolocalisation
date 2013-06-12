3ba52bf344290d740ec059a16df892f29b53714f
Rainer Müller <rainer@websimple.be>
2013-06-12 12:32:50 +0200

    le nouveau code

--- shop_geolocalisation_options.php
+++ shop_geolocalisation_options.php
@@ -12,23 +12,24 @@ $GLOBALS['taille_des_logs'] = 1000; // de 100ko au plus
 if($_COOKIE['spip_test']){
 	include_spip('inc/correspondances');
 	
-	$ip =$_SERVER['REMOTE_ADDR'];
+	$ip =$GLOBALS['ip'];
+    
 	
 	//ips de test
 	//USA
-	//$ip ='209.85.216.179';
+	$ip ='209.85.216.179';
 	//France
 	//$ip= '94.23.227.170'; 
 	 //Suisse	
 	//$ip= '212.147.58.119';
 	
-	
+
 	// Definir la langue selon l'origine (pays du visiteur)
 	if(!$_COOKIE['spip_lang']) determiner_langue_pays($ip);
 		
 		
 	if(!$_COOKIE['spip_devise']){
-		if(!$pays =$_COOKIE['spip_pays'])$pays=ip_pays($ip);	
+		if($pays =$_COOKIE['spip_pays'])$pays=ip_pays($ip);	
 		determiner_devise_pays($pays);
 		}	
 	
@@ -80,18 +81,13 @@ function determiner_langue_pays($ip){
 
 function ip_pays($ip){
 	$a=donnees_ip($ip);
-    
-    echo serialize($a);
-	preg_match("/\((.*?)\)/",$a['Country'],$match);
-	$pays= $match[1];	
+
+	//preg_match("/\((.*?)\)/",$a['Country'],$match);
+	$pays= $a['Country'];	
 	
-	if(!$_COOKIE['spip_pays'])spip_setcookie('spip_pays',$pays,time()+6*40*24*3600);	
+	if(!$_COOKIE['spip_pays'])spip_setcookie('spip_pays',$pays,time()+30*24*3600);	
 	
-	$pattern = '/^Unknown/';
-	preg_match($pattern, $pays, $matches);
-	if($erreur=$matches[0])spip_log("ip: $ip - code_pays:$pays",'detection_pays_erreur');
-	else spip_log("ip: $ip - code_pays:$pays",'detection_pays');
-	
+
 return $pays;
 }
 
@@ -100,105 +96,12 @@ return $pays;
 
 function donnees_ip($ip){
 
-	$url='http://api.hostip.info/get_html.php?ip='.$ip.'&position=true';
+	$url="http://api.easyjquery.com/ips/?ip=".$ip."&full=true"; 
  
 	$data=file_get_contents($url);
-
-	$a=array();
-	$keys=array('Country','City','Latitude','Longitude','IP');
-	$keycount=count($keys);
-	for ($r=0; $r < $keycount ; $r++)
-	{
-		$sstr= substr ($data, strpos($data, $keys[$r]), strlen($data));
-		if ( $r < ($keycount-1))
-			$sstr = substr ($sstr, 0, strpos($sstr,$keys[$r+1]));
-		$s=explode (':',$sstr);
-		$a[$keys[$r]] = trim($s[1]);
-	}
     
-    /*
- $ip = $_SERVER['REMOTE_ADDR']; 
- remember chmod 0777 for folder 'cache' 
-$file = "./cache/".$ip; 
-if(!file_exists($file)) { 
-    // request 
-    $json = file_get_contents("http://api.easyjquery.com/ips/?ip=".$ip."&full=true"); 
-    $f = fopen($file,"w+"); 
-    fwrite($f,$json); 
-    fclose($f); 
-} else { 
-    $json = file_get_contents($file); 
-} 
-
-$json = json_decode($json,true); 
-echo "<pre>"; 
-print_r($json); 
-     * 
-     * return     [ContinentCode] => NA
-    [ContinentName] => North America
-    [CountryCode2] => US
-    [CountryCode3] => USA
-    [COUNTRY] => US
-    [CountryName] => United States
-    [RegionName] => California
-    [CityName] => Garden Grove
-    [CityLatitude] => 33.7831
-    [CityLongitude] => -118.0271
-    [CountryLatitude] => 38
-    [CountryLongitude] => -98
-    [LocalTimeZone] => America/Los_Angeles
-    [REMOTE_ADDR] => 77.93.210.64
-    [HTTP_X_FORWARDED_FOR] => 
-    [CallingCode] => 1
-    [Population] => 307,212,123 (3)
-    [AreaSqKm] => 9,826,675 (4)
-    [GDP_USD] => 14.26 Trillion (1)
-    [Capital] => Washington, D.C.
-    [Electrical] => 120 V,60 Hz Type A Type B
-    [Languages] => English 82.1%, Spanish 10.7%, other Indo-European 3.8%, Asian and Pacific island 2.7%, other 0.7% (2000 census)
-    [Currency] => US Dollar (USD)
-    [Flag] => http://api.easyjquery.com/proips/flags/US.jpg
-     * 
-     * autres .http://chir.ag/projects/geoiploc/
-     * 
-http://ipinfodb.com/ipinfodb_api_code.php
-ipQuery = mysql_query("SELECT * FROM `ip_group_city` where `ip_start` <= INET_ATON('$ip') order by ip_start desc limit 1;", $con);
-$ipData = mysql_fetch_array($ipQuery);
-$nbResults = (bool)mysql_num_rows($ipQuery);
-$geolocationArr['Ip'] = $ip;
-if ($nbResults) {
-  $geolocationArr['Status'] = 'OK';
-} else {
-  $geolocationArr['Status'] = 'IP NOT FOUND IN DATABASE, SORRY!';
-}                     
-$geolocationArr['CountryCode'] = $ipData['country_code'];
-$geolocationArr['CountryName'] = utf8_encode($ipData['country_name']);
-$geolocationArr['RegionCode'] = $ipData['region_code'];
-$geolocationArr['RegionName'] = utf8_encode($ipData['region_name']);
-$geolocationArr['City'] = utf8_encode($ipData['city']);
-$geolocationArr['ZipPostalCode'] = $ipData['zipcode'];
-$geolocationArr['Latitude'] = $ipData['latitude'];
-$geolocationArr['Longitude'] = $ipData['longitude'];
+    $data=json_decode($data,true); 
  
-if (!$newTimezone) {
-  $geolocationArr['Timezone'] = $ipData['timezone'];
-  $geolocationArr['Gmtoffset'] = $ipData['gmtOffset'];
-  $geolocationArr['Dstoffset'] = $ipData['dstOffset'];
-} else {
-  if ($showNewTimezone) {
-    if ($nbResults) {
-      mysql_select_db('troquez', $con);
-      $tzQuery = mysql_query("SELECT tzd.gmtoff as gmtoff, tzd.isdst as isdst, tz.name as name FROM `timezones_data` tzd JOIN `timezones` tz ON tz.id = tzd.timezone WHERE tzd.timezone = (SELECT `timezone` FROM `fips_regions` WHERE `country_code` = '" . mysql_real_escape_string($ipData['country_code'], $con) . "' AND `code` = '" . mysql_real_escape_string($ipData['region_code'], $con) . "' ) AND tzd.start < UNIX_TIMESTAMP( now( ) ) ORDER BY tzd.start DESC LIMIT 1", $con);
-      $tzData = mysql_fetch_array($tzQuery);
-    }
-    $geolocationArr['TimezoneName'] = $tzData['name'];
-    $geolocationArr['Gmtoffset'] = $tzData['gmtoff'];
-    $geolocationArr['Isdst'] = $tzData['isdst'];
-  }
-}
-     * 
-     */
- 
-return $a;
+return $data;
 }
 ?>
